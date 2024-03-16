06985c4441272bb558d061913df316c2cbfdca5d
Liam Xu <liamxu24@outlook.com>
2024-03-15 23:53:22 -0700

    updated requirements

---
+++ page3.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import graphviz
+import json
+
+
+with open('typical_tasks_example_questions_graphs_merged.json', 'r') as f:
+    data = json.load(f)
+
+all_queries = data[2]['simulated_queries']
+all_graphs = data[2]['res_graphs']
+merged_graph = data[2]['merged_graphs']
+
+tabs = st.tabs(['Query 0', 'Query 1', 'Query 2', 'Query 3', 'Query 4', 'Merged'])
+
+for idx, (query, res) in enumerate(zip(all_queries, all_graphs)):
+    # Create multiple tabs
+    with tabs[idx]:
+      with st.container():
+          # Create a graphlib graph object
+          graph = graphviz.Digraph()
+
+          for edge in res[1]:
+              graph.edge(edge[0], edge[1], label=str(edge[2]))
+          st.write("## Query")
+          st.write(query)
+          st.write("## Causal Graph Created from Query")
+          st.graphviz_chart(graph)
+          st.write("## Causal Graph Node Explanation")
+          st.write(res[0])
+          st.write("## Causal Graph Reasoning")
+          st.write(res[2])
+
+with tabs[-1]:
+    with st.container():
+        # Create a graphlib graph object
+        graph = graphviz.Digraph()
+
+        for edge in merged_graph[1]:
+            graph.edge(edge[0], edge[1], label=str(edge[2]))
+        st.write("## Causal Graph Created from Queries")
+        st.graphviz_chart(graph)
+        st.write("## Causal Graph Node Explanation")
+        st.write(merged_graph[0])
+        st.write("## Causal Graph Reasoning")
+        st.write(merged_graph[2])
+
